f3182a11628a0937fd958171cf2f07ab273b5661
TanguyOrhon <tanguy.orhon32@gmail.com>
2025-02-22 16:48:00 +0100

    git init

---
+++ src/main/java/decouv/spring/DecSpringApplication.java
@@ -0,0 +1,13 @@
+package decouv.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/decouv/spring/DecSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package decouv.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
